8b4bd563893f767a87d81a5d35454f011c31ecc4
ariessa-deriv <>
2022-07-19 11:42:28 +0800

    Added coin configs

---
+++ pages/helpers/coinConfigs.js
@@ -0,0 +1,128 @@
+import bitcoin from "/images/bitcoin.png";
+import ethereum from "/images/ethereum.png";
+import tether from "/images/tether.png";
+import usdCoin from "/images/usdCoin.png";
+import bnb from "/images/bnb.png";
+import binanceUsd from "/images/binanceUsd.png";
+import xrp from "/images/xrp.png";
+import cardano from "/images/cardano.png";
+import solana from "/images/solana.png";
+import dogecoin from "/images/dogecoin.png";
+import polkadot from "/images/polkadot.png";
+import polygon from "/images/polygon.png";
+import shibaInu from "/images/shibaInu.png";
+import dai from "/images/dai.png";
+import avalanche from "/images/avalanche.png";
+import tron from "/images/tron.png";
+import bitcoin from "/images/bitcoin.png";
+import lidoStakedEther from "/images/lidoStakedEther.png";
+import leoToken from "/images/leoToken.png";
+import litecoin from "/images/litecoin.png";
+import ftx from "/images/ftx.png";
+import okb from "/images/okb.png";
+import uniswap from "/images/uniswap.png";
+import cronos from "/images/cronos.png";
+import chainlink from "/images/chainlink.png";
+import ethereumClassic from "/images/ethereumClassic.png";
+import nearProtocol from "/images/nearProtocol.png";
+import stellar from "/images/stellar.png";
+import cosmosHub from "/images/cosmosHub.png";
+import monero from "/images/monero.png";
+
+export const coinConfigs = [
+  { id: "bitcoin", name: "Bitcoin", symbol: "BTC", image: bitcoin },
+  { id: "ethereum", name: "Ethereum", symbol: "ETH", image: ethereum },
+  { id: "tether", name: "Tether", symbol: "USDT", image: tether },
+  { id: "usdCoin", name: "USD Coin", symbol: "USDC", image: usdCoin },
+  { id: "bnb", name: "BNB", symbol: "BNB", image: bnb },
+  { id: "binanceUsd", name: "Binance USD", symbol: "BUSD", image: binanceUsd },
+  { id: "xrp", name: "XRP", symbol: "XRP", image: xrp },
+  { id: "cardano", name: "Cardano", symbol: "ADA", image: cardano },
+  { id: "solana", name: "Solana", symbol: "SOL", image: solana },
+  { id: "dogecoin", name: "Dogecoin", symbol: "DOGE", image: dogecoin },
+  { id: "polkadot", name: "Polkadot", symbol: "DOT", image: polkadot },
+  { id: "polygon", name: "Polygon", symbol: "MATIC", image: polygon },
+  { id: "shibaInu", name: "Shiba Inu", symbol: "SHIB", image: shibaInu },
+  { id: "dai", name: "Dai", symbol: "DAI", image: dai },
+  { id: "avalanche", name: "Avalanche", symbol: "AVAX", image: avalanche },
+  { id: "tron", name: "Tron", symbol: "TRX", image: tron },
+  {
+    id: "lidoStakedEther",
+    name: "Lido Staked Ether",
+    symbol: "STETH",
+    image: lidoStakedEther,
+  },
+  {
+    id: "wrappedBitcoin",
+    name: "Wrapped Bitcoin",
+    symbol: "WBTC",
+    image: wrappedBitcoin,
+  },
+  { id: "leoToken", name: "LEO Token", symbol: "LEO", image: leoToken },
+  { id: "litecoin", name: "Litecoin", symbol: "LTC", image: litecoin },
+  { id: "ftx", name: "FTX", symbol: "FTT", image: ftx },
+  { id: "okb", name: "OKB", symbol: "OKB", image: okb },
+  { id: "uniswap", name: "Uniswap", symbol: "UNI", image: uniswap },
+  { id: "cronos", name: "Cronos", symbol: "CRO", image: cronos },
+  { id: "chainlink", name: "Chainlink", symbol: "LINK", image: chainlink },
+  {
+    id: "ethereumClassic",
+    name: "Ethereum Classic",
+    symbol: "ETC",
+    image: ethereumClassic,
+  },
+  {
+    id: "nearProtocol",
+    name: "NEAR Protocol",
+    symbol: "NEAR",
+    image: nearProtocol,
+  },
+  { id: "stellar", name: "Stellar", symbol: "XLM", image: stellar },
+  { id: "cosmosHub", name: "Cosmos Hub", symbol: "ATOM", image: cosmosHub },
+  { id: "monero", name: "Monero", symbol: "XMR", image: monero },
+  // { id: "algorand", name: "Algorand", symbol: "ALGO", image: algorand },
+  // {
+  //   id: "bitcoinCash",
+  //   name: "Bitcoin Cash",
+  //   symbol: "BCH",
+  //   image: bitcoinCash,
+  // },
+  // { id: "chain", name: "Chain", symbol: "XCN", image: chain },
+  // { id: "thetaFuel", name: "Theta Fuel", symbol: "TFUEL", image: thetaFuel },
+  // {
+  //   id: "internetComputer",
+  //   name: "Internet Computer",
+  //   symbol: "ICP",
+  //   image: internetComputer,
+  // },
+  // { id: "apeCoin", name: "ApeCoin", symbol: "APE", image: apeCoin },
+  // { id: "veChain", name: "VeChain", symbol: "VET", image: veChain },
+  // { id: "flow", name: "Flow", symbol: "FLOW", image: flow },
+  // { id: "theSandbox", name: "The Sandbox", symbol: "SAND", image: theSandbox },
+  // {
+  //   id: "decentraland",
+  //   name: "Decentraland",
+  //   symbol: "MANA",
+  //   image: decentraland,
+  // },
+  // { id: "tezos", name: "Tezos", symbol: "XTZ", image: tezos },
+  // { id: "hedera", name: "Hedera", symbol: "HBAR", image: hedera },
+  // { id: "quant", name: "Quant", symbol: "QNT", image: quant },
+  // { id: "elrond", name: "Elrond", symbol: "EGLD", image: elrond },
+  // { id: "filecoin", name: "Filecoin", symbol: "FIL", image: filecoin },
+  // { id: "frax", name: "Frax", symbol: "FRAX", image: frax },
+  // { id: "aave", name: "Aave", symbol: "AAVE", image: aave },
+  // {
+  //   id: "axieInfinity",
+  //   name: "Axie Infinity",
+  //   symbol: "AXS",
+  //   image: axieInfinity,
+  // },
+  // {
+  //   id: "thetaNetwork",
+  //   name: "Theta Network",
+  //   symbol: "THETA",
+  //   image: thetaNetwork,
+  // },
+  // { id: "trueUsd", name: "TrueUSD", symbol: "TUSD", image: trueUsd },
+];
