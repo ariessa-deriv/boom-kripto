a876e29f97f824a57d951ab08b066424546efc2c
Ng Shin Cheng <shin.cheng@besquare.com.my>
2022-07-19 17:52:31 +0800

    Updated code

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,13 @@
 import Dashboard from "./Dashboard";
-import NavBar from "./NavBar";
-import Watchlist from "./Watchlist";
-import Login from "./Login";
-import React from "react";
-import Loginn from "./login1";
+import NavBar from "../components/NavBar";
 
 export default function Home() {
   return (
     <div>
-      {/* <NavBar /> */}
-      <Loginn />
-      {/* { <Watchlist /> } */}
+      <NavBar />
+
+      <Dashboard />
+      {/* <Watchlist /> */}
     </div>
   );
 }
