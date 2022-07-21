3ce93df34946b109a2d1bc258bdd17207a741a75
ariessa-deriv <>
2022-07-21 19:20:00 +0800

    Change indigo colour to purple

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -92,7 +92,7 @@ const NavBar = () => {
               <div className="flex">
                 <div className="-ml-2 mr-2 flex items-center md:hidden">
                   {/* Mobile menu button */}
-                  <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                  <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-purple-500">
                     <span className="sr-only">Open main menu</span>
                     {open ? (
                       <XIcon className="block h-6 w-6" aria-hidden="true" />
@@ -110,7 +110,7 @@ const NavBar = () => {
                       <a
                         className={classNames(
                           currentRoute === `${item.href}`
-                            ? "border-indigo-500 text-gray-900"
+                            ? "border-purple-500 text-gray-900"
                             : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700",
 
                           "inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
@@ -166,7 +166,7 @@ const NavBar = () => {
                     as="a"
                     className={classNames(
                       currentRoute === `${item.href}`
-                        ? "bg-indigo-50 border-indigo-500 text-indigo-700"
+                        ? "bg-purple-50 border-purple-500 text-purple-700"
                         : "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700",
                       "block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6"
                     )}
