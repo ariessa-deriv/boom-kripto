38e866d38f6e610ad57bd84931f4a0036c4eaa4a
ariessa-deriv <>
2022-07-21 19:15:56 +0800

    Change colour og logout button

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -136,7 +136,7 @@ const NavBar = () => {
                   <div className="flex-shrink-0">
                     <button
                       type="button"
-                      className="relative inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-purple-600 shadow-sm hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                      className="relative inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-purple-600 shadow-sm hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500"
                       onClick={() => setShowSignUpModal(!showSignUpModal)}
                     >
                       <span>Sign Up</span>
@@ -148,7 +148,7 @@ const NavBar = () => {
                   <div className="flex-shrink-0">
                     <button
                       type="button"
-                      className="relative inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                      className="relative inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-purple-600 shadow-sm hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500"
                       onClick={() => handleLogout()}
                     >
                       <span>Log Out</span>
